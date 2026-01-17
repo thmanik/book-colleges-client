9d5e04c776ea1b8a17d9be6f394ac378c03a7884
thmanik <thmanik1241@gmail.com>
2026-01-18 00:07:04 +0600

    ScrollToTop

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -3,12 +3,14 @@ import { Outlet } from "react-router-dom";
 
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
+import ScrollToTop from "../components/ScrollToTop/ScrollToTop";
 const Main = () => {
     return (
-        <div className="max-w-7xl m-auto">
-            <Header></Header>
-            <Outlet></Outlet>
-            <Footer></Footer>
+        <div className="max-w-7xl m-auto min-h-screen">
+            <ScrollToTop />
+            <Header/>
+            <Outlet/>
+            <Footer/>
         </div>
     );
 };
